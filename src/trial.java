9b5caf20aab1db36c0fc16f4d1b9e6b50af2e4ac
mernamikhael <merna.mikhael@student.guc.edu.eg>
2018-10-10 15:39:40 +0200

    bend 2 images

--- src/trial.java
+++ src/trial.java
@@ -12,26 +12,40 @@ public class trial {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Imgcodecs imageCodecs = new Imgcodecs();
         Core core= new Core();
-//        // read figure 3
-//        Mat figure3 = imageCodecs.imread("A1I/Q1I2.jpg");
-////        Imgcodecs.imwrite("test.png", );
-//        // flip figure 3
-//        core.flip(figure3,figure3,1);
-//        //write the flipped image
-//        imageCodecs.imwrite("test.png",figure3);
-//        System.out.println("mat = " + figure3.height());
-//        
         
+        // read figure 3
+          Mat figure3 = imageCodecs.imread("A1I/Q1I2.jpg");
+        // flip figure 3
+        core.flip(figure3,figure3,1);
+        //write the flipped image 
+        imageCodecs.imwrite("flipped.png",figure3);
+//        System.out.println("mat = " + figure3.height());
+       
+        //read figure1
         Mat figure1 = imageCodecs.imread("A1I/Q1I1.png");
-        double alpha = 0.5;
-        double beta = 20;
+        //brightness and contrast
+        double alpha = 10;
+        double beta = 100;
         Mat destination = new Mat(figure1.rows(),figure1.cols(),figure1.type());
         figure1.convertTo(destination, -1, alpha, beta);
         imageCodecs.imwrite("test2.png",destination);
+        
+        //create final mat with figure 1 size as it is larger
+        Mat finalMat = new Mat(figure1.rows(),figure1.cols(),figure1.type());
 
-        System.out.println("mat = " + figure1.height());
-
+        // blending 0.5 of each figure
+        for(int row=0;row<figure1.rows();row++){
+        	for(int col=0;col<figure1.cols();col++){
+        		double[]fig1val= figure1.get(row, col);
+        		double[]fig3val= figure3.get(row, col);
+        		finalMat.put(row, col, new double[]{(fig3val[0]*0.5)+(fig1val[0]*0.5),(fig3val[1]*0.5)+(fig1val[1]*0.5),(fig3val[2]*0.5)+(fig1val[2]*0.5)});
+        	}
+        }
+        //write final image
+        imageCodecs.imwrite("blend.png",finalMat);
 
 }
+    
+    
     
 }
