394ccb96d04127b4b9579c295eb096bea762ea34
mernamikhael <merna.mikhael@student.guc.edu.eg>
2018-10-10 17:07:52 +0200

    blend

--- src/trial.java
+++ src/trial.java
@@ -3,6 +3,7 @@ import java.awt.BufferCapabilities.FlipContents;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -17,6 +18,8 @@ public class trial {
           Mat figure3 = imageCodecs.imread("A1I/Q1I2.jpg");
         // flip figure 3
         core.flip(figure3,figure3,1);
+        Imgproc.resize(figure3, figure3, new Size(1121,788 ));
+
         //write the flipped image 
         imageCodecs.imwrite("flipped.png",figure3);
 //        System.out.println("mat = " + figure3.height());
@@ -24,18 +27,35 @@ public class trial {
         //read figure1
         Mat figure1 = imageCodecs.imread("A1I/Q1I1.png");
         //brightness and contrast
-        double alpha = 10;
+        double alpha = 2;
         double beta = 100;
         Mat destination = new Mat(figure1.rows(),figure1.cols(),figure1.type());
-        figure1.convertTo(destination, -1, alpha, beta);
-        imageCodecs.imwrite("test2.png",destination);
-        
+//        figure1.convertTo(destination, -1, alpha, beta);
+        for(int row=0;row<figure1.rows();row++){
+        	for(int col=0;col<figure1.cols();col++){
+        		double[]fig1val= figure1.get(row, col);
+        		if(row==figure1.rows()/2)
+        		{   
+	        		if(alpha>0) alpha--;
+	        		if(beta>50)beta--;
+	        		System.out.println(alpha);
+	        		System.out.println(beta);
+	        	}
+        		destination.put(row, col, new double[]{(fig1val[0]*alpha)+beta,(fig1val[1]*alpha)+beta,
+        				(fig1val[2]*alpha)+beta});
+        	
+        	}
+        	}
+      imageCodecs.imwrite("bright.png",destination);
+
         //create final mat with figure 1 size as it is larger
+      
+      
         Mat finalMat = new Mat(figure1.rows(),figure1.cols(),figure1.type());
 
         // blending 0.5 of each figure
-        for(int row=0;row<figure1.rows();row++){
-        	for(int col=0;col<figure1.cols();col++){
+        for(int row=0;row<figure3.rows();row++){
+        	for(int col=0;col<figure3.cols();col++){
         		double[]fig1val= figure1.get(row, col);
         		double[]fig3val= figure3.get(row, col);
         		finalMat.put(row, col, new double[]{(fig3val[0]*0.5)+(fig1val[0]*0.5),(fig3val[1]*0.5)+(fig1val[1]*0.5),(fig3val[2]*0.5)+(fig1val[2]*0.5)});
